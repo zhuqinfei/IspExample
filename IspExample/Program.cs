745bf7ff51e00cb76aafabb75485398369b372c4
zhuqinfei <1586859762@qq.com>
2024-01-17 16:59:11 +0800

    接口没有隔离，每次都要改动类Driver

---
+++ IspExample/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IspExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new Driver(new Car());
+            driver.Drive();
+        }
+    }
+    class Driver
+    {
+        private IVehicle _vehicle;
+        public Driver(IVehicle vehicle)
+        {
+            _vehicle = vehicle;
+        }
+        public void Drive()
+        {
+            _vehicle.Run();
+        }
+    }
+    interface IVehicle
+    {
+        void Run();
+    }
+    class Car:IVehicle
+    {
+        public void Run()
+        {
+            Console.WriteLine("Car is running...");
+        }
+    }
+    class Truck:IVehicle
+    {
+        public void Run()
+        {
+            Console.WriteLine("Truck is running...");
+        }
+    }
+    interface ITank
+    {
+        void Fire();
+        void Run();
+    }
+    class LightTank:ITank
+    {
+        public void Fire() 
+        {
+            Console.WriteLine("Boom!");
+        }
+        public void Run()
+        {
+            Console.WriteLine("ka ka ka ...");
+        }
+    }
+    class MediumTank : ITank
+    {
+        public void Fire()
+        {
+            Console.WriteLine("Boom!!");
+        }
+        public void Run()
+        {
+            Console.WriteLine("ka!! ka!! ka!! ...");
+        }
+    }
+    class HeavyTank : ITank
+    {
+        public void Fire()
+        {
+            Console.WriteLine("Boom!!!");
+        }
+        public void Run()
+        {
+            Console.WriteLine("ka!!! ka!!! ka!!! ...");
+        }
+    }
+}
