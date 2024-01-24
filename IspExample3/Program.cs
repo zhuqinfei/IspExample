2f017e30d3ebb82c9dca80ca07a790bba210bf6d
zhuqinfei <1586859762@qq.com>
2024-01-25 01:31:54 +0800

    接口隔离原则3：显式接口实现

--- IspExample3/Program.cs
+++ IspExample3/Program.cs
@@ -10,11 +10,31 @@ namespace IspExample3
     {
         static void Main(string[] args)
         {
+            var wk = new WarmKiller();
+            //这样杀手功能就能大家都能看到，不是我们想要的，我们要隐藏杀手功能，
+            //当给杀手任务时候才访问这个功能
+            wk.Kill();
+            wk.Love();
         } 
     }
     interface IGentleman
     {
-
+        void Love();
+    }
+    interface IKiller
+    {
+        void Kill();
+    }
+    class WarmKiller:IGentleman,IKiller
+    {
+        public void Love()
+        {
+            Console.WriteLine("Let me kill the enemy...");
+        }
+        public void Kill()
+        {
+            Console.WriteLine("I will love you for ever...");
+        }
     }
 
 }
