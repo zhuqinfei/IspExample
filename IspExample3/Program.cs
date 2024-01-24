a57f4708fdced03ef6078ad4f177e6dee6dbb29b
zhuqinfei <1586859762@qq.com>
2024-01-25 01:24:43 +0800

    接口隔离原则3：显式接口实现

---
+++ IspExample3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IspExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        } 
+    }
+    interface IGentleman
+    {
+
+    }
+
+}
